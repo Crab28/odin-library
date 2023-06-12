84ad3f9ca0064ad60c5eeeee0ec2e8cf99efece7
Crab <crabcodes28@gmail.com>
2023-06-12 15:14:32 +0200

    Add book display functions

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.getInfo = function() {
-    return [this.title, this.author, this.pages, this.read];
+    return {
+        title: this.title, 
+        author: this.author,
+        pages: this.pages,
+        read: this.read
+    }
 }
 
 function addBookToLibrary(book) {
@@ -16,13 +21,45 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+    const cardContainer = document.getElementById('card-container');
+
     myLibrary.forEach(book => {
         const bookInfo = book.getInfo();
+
+        cardContainer.appendChild(createBookElement(bookInfo));
+
         console.log(bookInfo);
     });
 }
 
+function createBookElement(bookInfo) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('card');
+
+    const titleEl = document.createElement('h4');
+    titleEl.textContent = bookInfo.title;
+    const authorEl = document.createElement('h6');
+    authorEl.textContent = bookInfo.author;
+    const pagesEl = document.createElement('h6');
+    pagesEl.textContent = bookInfo.pages;
+    const readEl = document.createElement('h6');
+    readEl.textContent = bookInfo.read;
+
+    const elementsArray = [titleEl, authorEl, pagesEl, readEl];
+    
+    elementsArray.forEach(el => {
+        newDiv.appendChild(el);
+    });
+
+    return newDiv;
+}
+
 const theHobbit = new Book('The Hobbit', 'J.K.', '277', 'not read');
+const theHobbit2 = new Book('The Hobbit2', 'J.K.2', '477', 'read');
+const theHobbit3 = new Book('The Hobbit3', 'J.K.3', '377', 'not read');
 
 addBookToLibrary(theHobbit);
+addBookToLibrary(theHobbit2);
+addBookToLibrary(theHobbit3);
+
 displayBooks();
